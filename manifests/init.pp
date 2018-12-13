00b8c245631e5899a685ebcca74aa671328b8df7
Trey Dockendorf <tdockendorf@osc.edu>
2018-12-13 14:33:20 -0500

    Reduce retention of psacct logs to 14 days for all clusters A bit of cleanup and modernization for the psacct module

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,7 @@
 class psacct (
   $package_ensure       = 'present',
   $package_name         = $psacct::params::package_name,
+  $retention            = 31,
   $service_name         = $psacct::params::service_name,
   $service_ensure       = 'running',
   $service_enable       = true,
@@ -9,10 +10,38 @@ class psacct (
   $service_hasrestart   = $psacct::params::service_hasrestart,
 ) inherits psacct::params {
 
+  if $facts['service_provider'] == 'systemd' {
+    $logrotate_postrotate = [
+      'if /usr/bin/systemctl --quiet is-active psacct.service ; then',
+      '  /usr/sbin/accton /var/account/pacct | /usr/bin/grep -v "Turning on process accounting, file set to \'/var/account/pacct\'." | /usr/bin/cat',
+      'fi',
+    ]
+  } else {
+    $logrotate_postrotate = [
+      'if [ -f /var/lock/subsys/psacct ]; then',
+      '  /usr/sbin/accton /var/account/pacct',
+      'fi',
+    ]
+  }
+
   package { 'psacct':
     ensure => $package_ensure,
     name   => $package_name,
     notify => Service['psacct'],
+  }
+
+  logrotate::rule { 'psacct':
+    path          => '/var/account/pacct',
+    compress      => true,
+    delaycompress => true,
+    ifempty       => false,
+    rotate_every  => 'day',
+    rotate        => $retention,
+    create        => true,
+    create_mode   => '0600',
+    create_owner  => 'root',
+    create_group  => 'root',
+    postrotate    => $logrotate_postrotate,
   }
 
   service { 'psacct':
